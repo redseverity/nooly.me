7098441a06b00a658294baa0de5b5088a09b2e58
redseverity <red.severity723@passfwd.com>
2025-01-20 20:35:05 -0500

    feat(globals.css): add color palettes.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased font-poppins font-normal`}>
+      <body className={`antialiased font-poppins font-normal bg-dark-200`}>
         {children}
       </body>
     </html>
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -7,11 +7,44 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      dark: {
+        100: "var(--dark-1)",
+        200: "var(--dark-2)",
+        300: "var(--dark-3)",
+        400: "var(--dark-4)",
+        500: "var(--dark-5)",
+        600: "var(--dark-6)",
+        700: "var(--dark-7)",
+        800: "var(--dark-8)"
       },
+
+      white: {
+        100: "var(--white-1)",
+        200: "var(--white-2)",
+        300: "var(--white-3)",
+        400: "var(--white-4)",
+        500: "var(--white-5)",
+        600: "var(--white-6)",
+        700: "var(--white-7)",
+        800: "var(--white-8)"
+      },
+
+      rose: {
+        100: "var(--rose-1)",
+        200: "var(--rose-2)",
+        300: "var(--rose-3)"
+      },
+
+      green: {
+        600: "var(--green-1)"
+      },
+
+      red: {
+        600: "var(--red-1)"
+      }
+    },
+    extend: {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         inter: ['Inter', 'sans-serif']
