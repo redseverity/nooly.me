61c1a3ef65852c8e945f9a02a1063ac06ccd409e
redseverity <red.severity723@passfwd.com>
2025-01-31 18:30:23 -0500

    fix(header): update responsive classes of Header component.

--- apps/web/src/components/layouts/Header.tsx
+++ apps/web/src/components/layouts/Header.tsx
@@ -21,8 +21,11 @@ const Header = ({ className }: HeaderProps) => {
           flex justify-between items-center h-full
           screen-12:w-[65rem] screen-10:w-[94%] w-[88%]"
       >
-        <Link href="/">
-          <Logo />
+        <Link href="/" className="h-full flex items-center">
+          <Logo
+            className="max-[400px]:text-[15pt]"
+            classNameSmile="max-[400px]:left-[-3px] max-[400px]:top-[-33px] max-[400px]:w-[35px]"
+          />
         </Link>
 
         <div
@@ -35,7 +38,7 @@ const Header = ({ className }: HeaderProps) => {
             max-[1100px]:hidden flex justify-center
             items-center border border-dark-800
             rounded-full overflow-hidden h-[2.8rem]
-            w-min p-[0px]"
+            p-[0px] w-[400px]"
           >
             {["Home", "About", "Tools", "Contacts"].map((e, i) => {
               return (
@@ -46,11 +49,12 @@ const Header = ({ className }: HeaderProps) => {
                   draggable="false"
                   aria-label={`Navigate to the ${e} section`}
                   title={`Go to the ${e} section`}
-                  className="
-                  flex justify-center items-center
-                  hover:text-rose-200 hover:font-bold
+                  className={`
+                  flex flex-grow justify-center items-center
+                  hover:bg-dark-400
                   text-white-300 text-nowrap font-inter 
-                  font-semibold rounded-full w-[100px] h-[2.8rem]"
+                  font-semibold h-full
+                  `}
                 >
                   {e}
                 </Link>
@@ -60,9 +64,9 @@ const Header = ({ className }: HeaderProps) => {
 
           <div
             className="
-            screen-8:flex hidden
+            screen-8:flex hidden overflow-hidden
             bg-dark-200 rounded-full border-[1px]
-            border-dark-800 min-w-[200px] px-2 h-[2.8rem]"
+            border-dark-800 w-[200px] h-[2.8rem]"
           >
             {["Log In", "Sign Up"].map((e, i) => {
               const page = e.replace(" ", "").toLowerCase();
@@ -73,11 +77,12 @@ const Header = ({ className }: HeaderProps) => {
                   aria-label={`Navigate to the ${page} page`}
                   title={`Go to the ${page} page`}
                   draggable="false"
-                  className="
-                  flex font-inter text-white-300
-                  text-[12pt] hover:text-rose-200
-                  hover:font-bold font-semibold
-                  text-nowrap w-1/2 h-[2.8rem]"
+                  className={`
+                  flex flex-1 font-inter text-white-300
+                  text-[12pt] hover:bg-dark-400
+                  font-semibold text-nowrap h-full
+                  ${e == "Log In" ? "pl-1" : "pr-1"}
+                  `}
                 >
                   {e}
                 </Link>
@@ -91,22 +96,29 @@ const Header = ({ className }: HeaderProps) => {
             aria-label="Navigate to the tools page"
             title="Go to the tools page"
             className="
-              gap-1 max-[360px]:hidden flex
-              font-inter text-white-100 screen-8:text-[14pt] text-[11pt]
-              screen-8:h-[2.7rem] h-[2.4rem] screen-8:w-40 w-[8rem] py-2 ml-2 font-bold
+              min-[410px]:gap-1 max-[280px]:hidden flex
+              font-inter text-white-100 font-bold
+
+              min-[800px]:text-[14pt] min-[410px]:text-[11pt] text-[9pt]
+              min-[800px]:h-[2.7rem] min-[410px]:h-[2.5rem] h-[2.3rem]
+              min-[800px]:w-[10rem] min-[410px]:w-[8rem] w-[6rem]
+
               text-nowrap rounded-full bg-gradient-to-b
               from-rose-200 to-rose-300 hover:from-rose-100
               hover:to-rose-200"
           >
-            <div className="pl-1">Get Started</div>
-            <ChevronRight size={20} strokeWidth={3} />
+            <div className="pl-1">Try for Free</div>
+            <ChevronRight
+              size={20}
+              className="min-[410px]:stroke-[3px] stroke-[2px]"
+            />
           </Link>
 
           <div
             className="
               screen-8:w-[2.6rem] w-[2.2rem] screen-8:h-[2.6rem] h-[2.2rem]
-              max-[360px]:hidden flex justify-center items-center
-              rounded-full border-[1px] border-dark-800"
+              max-[380px]:hidden flex justify-center items-center 
+              rounded-full border-[1px] border-dark-800 hover:bg-dark-400"
           >
             <Moon className="stroke-none fill-white-100" />
           </div>
