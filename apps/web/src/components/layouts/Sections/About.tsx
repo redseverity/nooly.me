657b1d37e57a2abf528ef41c6b12a8c00258d772
redseverity <red.severity723@passfwd.com>
2025-02-26 22:18:17 -0500

    refactor(Sections): update Tools to support translations

--- apps/web/src/components/layouts/Sections/About.tsx
+++ apps/web/src/components/layouts/Sections/About.tsx
@@ -10,24 +10,24 @@ export const About = () => {
       className="flex scroll-mt-14 items-center justify-center gap-4 pb-4 pt-10 min-[600px]:gap-6"
     >
       <h2 className="dark:text-light-2 text-dark-2 text-nowrap pb-6 text-[7vw] font-semibold min-[600px]:text-[7vw] min-[800px]:text-[40pt]">
-        {t("About.h1")}
+        {t("About.h2")}
         <span className="text-rose-2">+</span>
       </h2>
 
       <div>
         <div className="flex w-[86vw] flex-col items-center justify-center gap-6 min-[330px]:w-[88vw] min-[750px]:w-[42rem]">
           <div className="grid w-full grid-cols-1 grid-rows-4 gap-6 min-[750px]:grid-cols-2 min-[750px]:grid-rows-2">
-            {t.raw("About.SmallCardTexts").map((e: string, i: number) => {
+            {t.raw("About.SmallCardTexts").map((text: string, i: number) => {
               return (
                 <Card
                   className="dark:bg-dark-3 bg-light-3 text-nowrap px-6 py-4 min-[500px]:py-6"
                   key={i}
                 >
                   <h3 className="gradient-text-rose text-[5vw] font-semibold min-[500px]:text-[19pt]">
-                    {e[0]}
+                    {text[0]}
                   </h3>
                   <p className="dark:text-light-2 text-dark-2 text-[3.5vw] font-semibold min-[600px]:text-[13pt]">
-                    {e[1]}
+                    {text[1]}
                   </p>
                 </Card>
               );
--- apps/web/src/components/layouts/Sections/Tools.tsx
+++ apps/web/src/components/layouts/Sections/Tools.tsx
@@ -11,37 +11,40 @@ import {
 import { useTranslations } from "next-intl";
 
 export const Tools = () => {
-  const tools: Array<[string, LucideIcon?]> = [
-    ["Dowload Video", FileVideo],
-    ["Dowload Music", FileMusic],
-    ["Image Converter", FileImage],
-    ["Resize Image", Scaling],
-    ["QR Code Generator", QrCode],
-    ["Remove metadate", FileText],
+  const IconsList: LucideIcon[] = [
+    FileVideo,
+    FileMusic,
+    FileImage,
+    Scaling,
+    QrCode,
+    FileText,
   ];
 
-  const t = useTranslations("Navegate");
+  const t = useTranslations();
 
   return (
     <Section
-      id={t.raw("OnPage")[2]}
+      id={t.raw("Navegate.OnPage")[2]}
       className="scroll-mt-16 gap-2 pb-4 pt-10 min-[600px]:gap-6"
     >
       <h2 className="dark:text-light-2 text-dark-2 text-nowrap pb-8 text-3xl font-semibold min-[600px]:text-5xl">
-        Start Now<span className="text-rose-2">!</span>
+        {t("Tools.h2")}
+        <span className="text-rose-2">!</span>
       </h2>
       <div className="grid w-[86vw] grid-cols-2 gap-4 max-[600px]:grid-cols-1 min-[330px]:w-[88vw] min-[750px]:w-[42rem]">
-        {tools.map(([label, Icon], i) => {
+        {t.raw("Tools.SmallCardTexts").map((text: string, i: number) => {
+          const IconComponent = IconsList[i];
+
           return (
             <Card
               className="dark:bg-dark-3 hover:dark:bg-dark-6 bg-light-3 hover:bg-dark-1 text-dark-2 hover:text-light-1 dark:text-light-2 flex items-center justify-center py-4"
               key={i}
             >
               <span className="flex w-[88%] items-center gap-1 text-nowrap text-sm font-semibold leading-none min-[250px]:gap-2 min-[600px]:gap-3 min-[600px]:text-lg">
-                {Icon && (
-                  <Icon className="stroke-rose-2 size-4 min-[600px]:size-6" />
+                {IconComponent && (
+                  <IconComponent className="stroke-rose-2 size-4 min-[600px]:size-6" />
                 )}
-                {label}
+                {text}
               </span>
             </Card>
           );
