ec850d86df32909998f7edf18ce18d4ef2076a18
redseverity <red.severity723@passfwd.com>
2025-02-26 22:33:10 -0500

    refactor(Sections): update Contacts to support translations

--- apps/web/src/components/layouts/Sections/Contacts.tsx
+++ apps/web/src/components/layouts/Sections/Contacts.tsx
@@ -1,21 +1,35 @@
 import { Navegate, Section, Icons } from "@/components/ui";
+import clsx from "clsx";
 import { ChevronRight, AtSign } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { useLocale } from "next-intl";
 import Link from "next/link";
 
 export const Contacts = () => {
-  const t = useTranslations("Navegate");
+  const t = useTranslations();
+  const locale = useLocale();
 
   return (
-    <Section id={t.raw("OnPage")[3]} className="scroll-mt-14 border-none py-12">
-      <h2 className="dark:text-light-2 text-dark-2 text-nowrap pb-6 text-center text-4xl font-semibold leading-tight min-[600px]:text-5xl min-[600px]:leading-tight">
-        Get Started with
+    <Section
+      id={t.raw("Navegate.OnPage")[3]}
+      className="scroll-mt-14 border-none py-12"
+    >
+      <h2
+        className={clsx(
+          "dark:text-light-2 text-dark-2 text-nowrap pb-6 text-center font-semibold leading-tight min-[600px]:text-5xl min-[600px]:leading-tight",
+          locale === "pt" ? "text-[2.5rem]" : "text-4xl",
+        )}
+      >
+        {t.raw("Contacts.h2")[0]}
         <br />
-        Nooly Today
+        {t.raw("Contacts.h2")[1]}
       </h2>
 
       <div className="flex items-center justify-center gap-2">
-        <Navegate.ToTools text="Get started now" className="pl-3 pr-2">
+        <Navegate.ToTools
+          text={t.raw("Navegate.ToTools")[1]}
+          className="pl-3 pr-2"
+        >
           <ChevronRight className="size-5 min-[500px]:size-6" />
         </Navegate.ToTools>
 
